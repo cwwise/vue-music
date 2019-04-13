dcd7bd73df3ac4b92993860f851644d811c80fb7
chenwei <wei18810109052@163.com>
2019-04-13 11:05:53 +0800

    添加slider组件

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,15 +1,51 @@
 <template>
-  <div>
-    推荐
+  <div class="recommend">
+    <div v-if="recommends.length" class="slider-wrapper">
+      <slider>
+        <div v-for="item in recommends" :key="item.id">
+          <a :href="item.linkUrl">
+            <img :src="item.picUrl" alt="">
+          </a>
+        </div>
+      </slider>
+    </div>
+
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+  import Slider from '@/base/slider/slider'
+  import {getRecommend} from '@/api/recommend'
+  import {ERR_OK} from '@/api/config'
+
+  export default {
+    data() {
+      return {
+        recommends: []
+      }
+    },
+    created() {
+      this._getRecommend()
+    },
+    components: {
+      Slider
+    },
+    methods: {
+      _getRecommend() {
+        getRecommend().then((res) => {
+          if (res.code == ERR_OK) {
+            this.recommends = res.data.slider;
+          }
+        })
+      }
+    }
+  }
 </script>
 
 <style lang="stylus" scoped>
 
+    .slider-wrapper
+        position: relative
+        width: 100%
+        overflow: hidden
 </style>
